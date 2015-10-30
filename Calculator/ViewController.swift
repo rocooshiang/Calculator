b9fe6373a18a1a00fbcd6d7fe607a44b4f385c59
rocooshiang <rocoo.shaing@gmail.com>
2015-10-31 03:19:30 +0800

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Geosat-RD01 on 2015/10/31.
+//  Copyright © 2015年 Geosat-RD01. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
